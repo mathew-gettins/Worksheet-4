ff057668b12a1092d68e3a5c794898062ab60bca
Mathew Gettins <S00240421@atu.ie>
2023-03-11 14:22:58 +0000

    Add project files.

---
+++ Question1/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Text;
+
+namespace Question1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            string name;
+            string idNumber;
+            double hoursWorked;
+            double hourlyPayRate;
+
+            // Get employee name and validate
+            do
+            {
+                Console.Write("Enter employee name: ");
+                name = Console.ReadLine();
+            } while (!ValidateName(name));
+
+            // Get employee ID number and validate
+            do
+            {
+                Console.Write("Enter employee ID number: ");
+                idNumber = Console.ReadLine();
+            } while (!ValidateIdNumber(idNumber));
+
+            // Get hours worked and validate
+            hoursWorked = GetValidDoubleInput("Enter hours worked: ", "Hours worked must be a number between 10 and 50.", 10, 50);
+
+            // Get hourly pay rate and validate
+            hourlyPayRate = GetValidDoubleInput("Enter hourly pay rate: ", "Hourly pay rate must be a number between 10 and 65.", 10, 65);
+
+            // Calculate tax and take home pay
+            double grossPay = CalculateGrossPay(hoursWorked, hourlyPayRate);
+            double taxRate = CalculateTaxRate(grossPay);
+            double taxAmount = CalculateTaxAmount(grossPay, taxRate);
+            double takeHomePay = CalculateTakeHomePay(grossPay, taxAmount);
+
+            // Display results
+            Console.WriteLine("Employee name: {0}", name);
+            Console.WriteLine("Employee ID number: {0}", idNumber);
+            Console.WriteLine("Gross pay: {0:C}", grossPay);
+            Console.WriteLine("Tax rate: {0:P}", taxRate);
+            Console.WriteLine("Tax amount: {0:C}", taxAmount);
+            Console.WriteLine("Take home pay: {0:C}", takeHomePay);
+
+            Console.ReadLine();
+        }
+
+        static bool ValidateName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                Console.WriteLine("Employee name is required. Please try again.");
+                return false;
+            }
+            return true;
+        }
+
+        static bool ValidateIdNumber(string idNumber)
+        {
+            if (string.IsNullOrWhiteSpace(idNumber) || idNumber.Length != 6 || !idNumber.StartsWith("E"))
+            {
+                Console.WriteLine("Employee ID number must be 6 characters long and start with 'E'. Please try again.");
+                return false;
+            }
+            return true;
+        }
+
+        static double GetValidDoubleInput(string prompt, string errorMessage, double minValue, double maxValue)
+        {
+            double inputValue;
+            bool isInputValid;
+
+            do
+            {
+                Console.Write(prompt);
+                if (double.TryParse(Console.ReadLine(), out inputValue) && inputValue >= minValue && inputValue <= maxValue)
+                {
+                    isInputValid = true;
+                }
+                else
+                {
+                    Console.WriteLine(errorMessage);
+                    isInputValid = false;
+                }
+            } while (!isInputValid);
+
+            return inputValue;
+        }
+
+        static double CalculateGrossPay(double hoursWorked, double hourlyPayRate)
+        {
+            return hoursWorked * hourlyPayRate;
+        }
+
+        static double CalculateTaxRate(double grossPay)
+        {
+            if (grossPay <= 3000)
+            {
+                return 0.0;
+            }
+            else if (grossPay <= 34000)
+            {
+                return 0.2;
+            }
+            else
+            {
+                return 0.4;
+            }
+        }
+
+        static double CalculateTaxAmount(double grossPay, double taxRate)
+        {
+            return grossPay * taxRate;
+        }
+
+        static double CalculateTakeHomePay(double grossPay, double taxAmount)
+        {
+            return grossPay - taxAmount;
+        }
+    }
+}
---
+++ Question2/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Input string to test the methods
+        string inputString = "xyxyxy Hello World xyxy";
+
+        // Test the methods
+        Console.WriteLine(CountXYCharacters(inputString));
+        Console.WriteLine(Alternate3Characters(inputString));
+        Console.WriteLine(StartsWithNumeric(inputString));
+        Console.WriteLine(InsertHyphens(inputString));
+        Console.WriteLine(StripSpecialCharacters(inputString));
+        Console.WriteLine(EndsWithCom(inputString));
+    }
+
+    // This method returns the number of times 'x' or 'y' appears in a string
+    // Input: a string called "inputString"
+    // Output: an integer representing the count of 'x' and 'y' characters
+    public static int CountXYCharacters(string inputString)
+    {
+        int count = 0;
+
+        // Loop through each character in the string
+        for (int i = 0; i < inputString.Length; i++)
+        {
+            // If the current character is 'x' or 'y', increment the count
+            if (inputString[i] == 'x' || inputString[i] == 'y')
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    // This method returns a new string made up of every alternate 3 characters
+    // Input: a string called "inputString"
+    // Output: a new string made up of every alternate 3 characters
+    public static string Alternate3Characters(string inputString)
+    {
+        string result = "";
+
+        // Loop through each group of 3 characters in the string
+        for (int i = 0; i < inputString.Length; i += 6)
+        {
+            // Add the first character of the group to the result
+            result += inputString[i];
+
+            // If the second character of the group exists, add it to the result
+            if (i + 2 < inputString.Length)
+            {
+                result += inputString[i + 2];
+            }
+
+            // If the third character of the group exists, add it to the result
+            if (i + 4 < inputString.Length)
+            {
+                result += inputString[i + 4];
+            }
+        }
+
+        return result;
+    }
+
+    // This method returns true if the string starts with a numeric character
+    // Input: a string called "inputString"
+    // Output: a boolean indicating whether the string starts with a numeric character
+    public static bool StartsWithNumeric(string inputString)
+    {
+        // Check if the first character is a numeric character
+        return Char.IsDigit(inputString[0]);
+    }
+
+    // This method returns a new string with a '-' inserted after every third character
+    // Input: a string called "inputString"
+    // Output: a new string with '-' inserted after every third character
+    public static string InsertHyphens(string inputString)
+    {
+        string result = "";
+
+        // Loop through each character in the string
+        for (int i = 0; i < inputString.Length; i++)
+        {
+            // If this is the third, sixth, ninth, etc. character, add a hyphen to the result
+            if (i > 0 && (i + 1) % 3 == 0)
+            {
+                result += '-';
+            }
+
+            // Add the current character to the result
+            result += inputString[i];
+        }
+
+        return result;
+    }
+
+    // This method returns a new string with special characters removed
+    // Input: a string called "inputString"
+    // Output:
+    public static string StripSpecialCharacters(string inputString)
+    {
+        string result = "";
+
+        // Loop through each character in the string
+        for (int i = 0; i < inputString.Length; i++)
+        {
+            // If the current character is not a special character, add it to the result
+            if (inputString[i] != '$' && inputString[i] != '%' && inputString[i] != ',' && inputString[i] != ' ')
+            {
+                result += inputString[i];
+            }
+        }
+
+        return result;
+    }
+
+    // This method returns true if the string ends with ".com"
+    // Input: a string called "inputString"
+    // Output: a boolean indicating whether the string ends with ".com"
+    public static bool EndsWithCom(string inputString)
+    {
+        // Check if the last four characters of the string are ".com"
+        return inputString.Length >= 4 && inputString.Substring(inputString.Length - 4) == ".com";
+    }
+}
